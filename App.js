053a046c7f7c2ac0245dfcf92bf2580369eb3765
Patrin <patrin.sinteppadon@gmail.com>
2021-02-24 13:51:51 -0800

    work on package.json (ITS DEF BROKEN THO)

--- App.js
+++ App.js
@@ -1,21 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
+// React Native Bottom Navigation
+// https://aboutreact.com/react-native-bottom-navigation/
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import firebase from 'firebase/app';
+// import 'firebase/database';
+
+// following tutorial from https://aboutreact.com/react-native-bottom-navigation/
+import {
+  NavigationContainer
+} from '@react-navigation/native';
+import {
+  createStackNavigator
+} from '@react-navigation/stack';
+import {
+  createBottomTabNavigator
+} from '@react-navigation/bottom-tabs';
+import
+ MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from './Components/HomeScreen.js' // TODO: rename to homescreen
+import UserScreen from './Components/UserScreen.js';
+import DetailsScreen from './pages/DetailsScreen';
+
+// instantiate Navigators
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+/* 
+  schema
+  - 2 tabs on our bottom nav: HomeTab, and UserTab
+  - 2 stacks to supplement the tabs: HomeStack, and UserStack
+*/
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#42f44b'},
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' }
+      }}>
+      <Stack.Screen 
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Home Page'}} 
+      /> 
+      <Stack.Screen 
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: 'Details Page'}}/>
+    </Stack.Navigator>
+  );
+}
+
+// seems like more of a backend manifest. Tab Navigator is the frontend part
+// of the navigation, but we send this info along to Settings bc the Settings
+// page will need to navigate you to Details and Profiles if you click on
+// the corresponding button while you're there
+function UserStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="User"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#42f44b' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' }
+      }}>
+      <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{ title: 'User Profile' }}/>
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons 
+                name="home"
+                color={color}
+                size={size}
+              />
+            ),
+          }}  
+        />
+        <Tab.Screen
+          name="UserStack"
+          component={UserStack}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons 
+                name="human"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
+
+  // // Initialize Firebase
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyAYH0zqdMHO0mhj0ee_7_kZyIEtn7vaOHA",
+  //   authDomain: "capstone-lilo.firebaseapp.com",
+  //   databaseURL: "https://capstone-lilo-default-rtdb.firebaseio.com",
+  //   projectId: "capstone-lilo",
+  //   storageBucket: "capstone-lilo.appspot.com",
+  //   messagingSenderId: "457024640739",
+  //   appId: "1:457024640739:web:8e31884b8a6b002a8357d6",
+  //   measurementId: "G-L2BJQKHLZP"
+  // };
+
+  // firebase.initializeApp(firebaseConfig);
---
+++ Components/HomeScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View class="main-page-container" style={{ flex: 1, padding: 16,}}>
+            <Text style={styles.titleText}>Lilo</Text>
+            <Text style={styles.welcomeText}>Welcome, [Username]!</Text>
+            <View style={styles.buttonContainer}>
+                <View style={styles.addSpacing}>
+                    <Button 
+                        title="Profile" 
+                        color="steelblue" 
+                        onPress={
+                            () => navigation.navigate(
+                                'UserStack', { screen: 'User' }
+                            )} />
+                </View>
+                <View style={styles.addSpacing}>
+                    <Button title="call Translator" color="steelblue" onPress={() => alert("Starting call...")}/>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        color: 'darkblue',
+        fontSize: 50,
+        textAlign: 'center'
+    },
+    welcomeText: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        textAlign: 'center'
+    },
+    buttonContainer: {
+        marginTop: 50,
+    },
+    addSpacing: {
+        marginTop: 5,
+        marginBottom: 5,
+    }
+  });
+
+export default HomeScreen;
