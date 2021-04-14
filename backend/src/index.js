05be792d465ceb72a16b19a0549381d8a680ea87
Paola Vanegas <pvdt@uw.edu>
2021-04-14 14:59:51 -0700

    create simple node server for calling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,77 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+
+// const port = 4000;
+
+// const http = require("http");
+// const server = http.createServer(app);
+
+// const io = require("socket.io")(server, { origins: "*:*" });
+// app.use(express.static(__dirname + "/public"));
+
+// io.sockets.on("error", (e) => console.log(e));
+// server.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// let broadcaster;
+
+// io.sockets.on("connection", (socket) => {
+//   socket.on("broadcaster", () => {
+//     broadcaster = socket.id;
+//     socket.broadcast.emit("broadcaster");
+//   });
+//   socket.on("watcher", () => {
+//     socket.to(broadcaster).emit("watcher", socket.id);
+//   });
+//   socket.on("disconnect", () => {
+//     socket.to(broadcaster).emit("disconnectPeer", socket.id);
+//   });
+//   socket.on("offer", (id, message) => {
+//     socket.to(id).emit("offer", socket.id, message);
+//   });
+//   socket.on("answer", (id, message) => {
+//     socket.to(id).emit("answer", socket.id, message);
+//   });
+//   socket.on("candidate", (id, message) => {
+//     socket.to(id).emit("candidate", socket.id, message);
+//   });
+//   socket.on('comment', (id, message) => {
+//     socket.to(id).emit("comment", socket.id, message);
+//   });
+// });
+
+const express = require("express")
+const http = require("http")
+const socketio = require("socket.io")
+const morgan = require("morgan")
+
+// used to generate unique id for each peer js connection from client
+const {ExpressPeerServer} = require("peer")
+
 const app = express();
+app.use(express.json())
 
-const port = 4000;
+// create server
+const server = http.createServer(app)
+const io = socketio(server).sockets
 
-const http = require("http");
-const server = http.createServer(app);
+// function generates custom unique ID 
+const customIDGenerator = () => 
+  (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
 
-const io = require("socket.io")(server, { origins: "*:*" });
-app.use(express.static(__dirname + "/public"));
+// create peer server
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/',
+  generateClientId: customIDGenerator,
+})
 
-io.sockets.on("error", (e) => console.log(e));
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+// TODO: change path of 'mypeer'
+app.use("/mypeer", peerServer)
 
-let broadcaster;
+// TODO MAYBE: update function on connection & add more (disconnected, etc.)
+io.on('connection', function(socket){
+  console.log('socket connected')
+}) 
 
-io.sockets.on("connection", (socket) => {
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
-  });
-  socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id);
-  });
-  socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
-  });
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message);
-  });
-  socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message);
-  });
-  socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message);
-  });
-  socket.on('comment', (id, message) => {
-    socket.to(id).emit("comment", socket.id, message);
-  });
-});
+const port = process.env.port || 5000;
+server.listen(port, () => console.log(`Server is running on port: ${port}`))
